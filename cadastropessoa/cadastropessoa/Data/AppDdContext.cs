0afa9c0c0e22beb5aeb5283feae4f58b27f308ad
sarahfs2007 <124725490+sarahfs2007@users.noreply.github.com>
2024-07-31 14:14:36 -0300

    Add files via upload

---
+++ cadastropessoa/cadastropessoa/Data/AppDdContext.cs
@@ -0,0 +1,13 @@
+﻿using cadastropessoa.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace cadastropessoa.Data;
+
+public class AppDdContext : DbContext
+{
+    public AppDdContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    DbSet<Pessoa> Pessoas { get; set; } = null!;
+}
---
+++ cadastropessoa/cadastropessoa/Models/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿namespace cadastropessoa.Models;
+
+public class Pessoa
+{
+    public int Id { get; set; }
+    
+    public string Nome { get; set; } = string.Empty;
+  
+    public string Cpf { get; set; } = string.Empty;
+   
+    public string Email { get; set; } = string.Empty;
+}
